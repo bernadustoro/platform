6ac0ff85aabec59a12fe5f56d7519289180d3270
bernadustoro <bernadustoro@gmail.com>
2024-03-25 22:46:07 +0700

    nambah section news

--- TugasUTS/latihan4.js
+++ TugasUTS/latihan4.js
@@ -1,13 +1,19 @@
 var home = document.getElementById('pills-home-tab');
-var tentangBtn = document.getElementById('pills-news-tab');
+var newsBtn = document.getElementById('pills-news-tab');
+var scheduleBtn = document.getElementById('pills-schedule-tab');
 var kontenhome = document.getElementById('pills-home');
 var kontennews = document.getElementById('pills-news');
+var kontenschedule = document.getElementById('pills-schedule');
 
 var homecopy = kontenhome.cloneNode(true);
-tentangBtn.addEventListener('click', function() {
+newsBtn.addEventListener('click', function() {
     kontenhome.innerHTML = kontennews.innerHTML;
 });
 
 home.addEventListener('click', function() {
     kontenhome.innerHTML = homecopy.innerHTML;
+});
+
+scheduleBtn.addEventListener('click', function() {
+    kontenhome.innerHTML = kontenschedule.innerHTML;
 });
