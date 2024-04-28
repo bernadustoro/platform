8d9f4558202d501b3157456df0f57cb58a63472a
bernadustoro <bernadustoro@gmail.com>
2024-04-28 20:52:44 +0700

    edit title head

--- latihan4/todoList.php
+++ latihan4/todoList.php
@@ -47,7 +47,7 @@ $text = query($username);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>TodoList</title>
     <link rel="stylesheet" href="todo.css">
 </head>
 
